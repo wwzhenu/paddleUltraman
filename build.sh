d9a2b12b17007d7377eb11accf75b19d9b6abab6
marin <wangwenzeng@tal.com>
2022-08-14 19:48:59 +0800

    add bash

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+# 安装paddlepaddle
+pip3 install paddlepaddle -i https://mirror.baidu.com/pypi/simple
+# 安装依赖
 pip3 install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 cd deploy
 mkdir models
@@ -7,3 +10,7 @@ wget https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/rec/models/infere
 # 下载识别inference模型并解压
 wget https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/rec/models/inference/product_ResNet50_vd_aliproduct_v1.0_infer.tar && tar -xf product_ResNet50_vd_aliproduct_v1.0_infer.tar
 wget https://paddle-imagenet-models-name.bj.bcebos.com/dygraph/rec/models/inference/cartoon_rec_ResNet50_iCartoon_v1.0_infer.tar && tar -xf cartoon_rec_ResNet50_iCartoon_v1.0_infer.tar
+# build vector
+cd ..
+cd vector_search
+make
